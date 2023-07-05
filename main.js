2dbaaa7c7e38c7243369f60e0c495fb8c22d50b2
groove <vacation555@gmail.com>
2023-07-05 13:16:34 +0900

    fix

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ import { isAfter, addDays } from "date-fns";
 const now = new Date();
 const threeDaysAgo = addDays(now, -3);
 console.log(isAfter(now, threeDaysAgo));
+if (threeDaysAgo) {
+  const title = document.getElementById("js-title");
+  title.textContent = "date-fnsが使えました";
+}
